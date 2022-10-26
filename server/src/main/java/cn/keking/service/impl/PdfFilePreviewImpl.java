5efe4343f311d7918be74e37db1f9d733f78e632
zhongyj <zyj8822@126.com>
2022-10-26 10:36:26 +0800

    添加打印日志

--- server/src/main/java/cn/keking/service/impl/PdfFilePreviewImpl.java
+++ server/src/main/java/cn/keking/service/impl/PdfFilePreviewImpl.java
@@ -7,78 +7,88 @@ import cn.keking.service.FilePreview;
 import cn.keking.utils.DownloadUtils;
 import cn.keking.service.FileHandlerService;
 import cn.keking.web.filter.BaseUrlFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import java.util.List;
 
 /**
- * Created by kl on 2018/1/17.
- * Content :处理pdf文件
+ * Created by kl on 2018/1/17. Content :处理pdf文件
  */
 @Service
 public class PdfFilePreviewImpl implements FilePreview {
 
-    private final FileHandlerService fileHandlerService;
-    private final OtherFilePreviewImpl otherFilePreview;
-    private static final String FILE_DIR = ConfigConstants.getFileDir();
+	private final FileHandlerService fileHandlerService;
+	private final OtherFilePreviewImpl otherFilePreview;
+	private static final String FILE_DIR = ConfigConstants.getFileDir();
 
-    public PdfFilePreviewImpl(FileHandlerService fileHandlerService, OtherFilePreviewImpl otherFilePreview) {
-        this.fileHandlerService = fileHandlerService;
-        this.otherFilePreview = otherFilePreview;
-    }
+	private static final Logger log = LoggerFactory.getLogger(PdfFilePreviewImpl.class);
 
-    @Override
-    public String filePreviewHandle(String url, Model model, FileAttribute fileAttribute) {
-        String fileName = fileAttribute.getName();
-        String officePreviewType = fileAttribute.getOfficePreviewType();
-        String baseUrl = BaseUrlFilter.getBaseUrl();
-        String pdfName = fileName.substring(0, fileName.lastIndexOf(".") + 1) + "pdf";
-        String outFilePath = FILE_DIR + pdfName;
-        if (OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_IMAGE.equals(officePreviewType) || OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_ALL_IMAGES.equals(officePreviewType)) {
-            //当文件不存在时，就去下载
-            if (!fileHandlerService.listConvertedFiles().containsKey(pdfName) || !ConfigConstants.isCacheEnabled()) {
-                ReturnResponse<String> response = DownloadUtils.downLoad(fileAttribute, fileName);
-                if (response.isFailure()) {
-                    return otherFilePreview.notSupportedFile(model, fileAttribute, response.getMsg());
-                }
-                outFilePath = response.getContent();
-                if (ConfigConstants.isCacheEnabled()) {
-                    // 加入缓存
-                    fileHandlerService.addConvertedFile(pdfName, fileHandlerService.getRelativePath(outFilePath));
-                }
-            }
-            List<String> imageUrls = fileHandlerService.pdf2jpg(outFilePath, pdfName, baseUrl);
-            if (imageUrls == null || imageUrls.size() < 1) {
-                return otherFilePreview.notSupportedFile(model, fileAttribute, "pdf转图片异常，请联系管理员");
-            }
-            model.addAttribute("imgurls", imageUrls);
-            model.addAttribute("currentUrl", imageUrls.get(0));
-            if (OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_IMAGE.equals(officePreviewType)) {
-                return OFFICE_PICTURE_FILE_PREVIEW_PAGE;
-            } else {
-                return PICTURE_FILE_PREVIEW_PAGE;
-            }
-        } else {
-            // 不是http开头，浏览器不能直接访问，需下载到本地
-            if (url != null && !url.toLowerCase().startsWith("http")) {
-                if (!fileHandlerService.listConvertedFiles().containsKey(pdfName) || !ConfigConstants.isCacheEnabled()) {
-                    ReturnResponse<String> response = DownloadUtils.downLoad(fileAttribute, pdfName);
-                    if (response.isFailure()) {
-                        return otherFilePreview.notSupportedFile(model, fileAttribute, response.getMsg());
-                    }
-                    model.addAttribute("pdfUrl", fileHandlerService.getRelativePath(response.getContent()));
-                    if (ConfigConstants.isCacheEnabled()) {
-                        // 加入缓存
-                        fileHandlerService.addConvertedFile(pdfName, fileHandlerService.getRelativePath(outFilePath));
-                    }
-                } else {
-                    model.addAttribute("pdfUrl", pdfName);
-                }
-            } else {
-                model.addAttribute("pdfUrl", url);
-            }
-        }
-        return PDF_FILE_PREVIEW_PAGE;
-    }
+	public PdfFilePreviewImpl(FileHandlerService fileHandlerService, OtherFilePreviewImpl otherFilePreview) {
+		this.fileHandlerService = fileHandlerService;
+		this.otherFilePreview = otherFilePreview;
+	}
+
+	@Override
+	public String filePreviewHandle(String url, Model model, FileAttribute fileAttribute) {
+		String fileName = fileAttribute.getName();
+		String officePreviewType = fileAttribute.getOfficePreviewType();
+		String baseUrl = BaseUrlFilter.getBaseUrl();
+		String pdfName = fileName.substring(0, fileName.lastIndexOf(".") + 1) + "pdf";
+		String outFilePath = FILE_DIR + pdfName;
+		log.info("fileName:{} pdfName:{} outFilePath:{} officePreviewType:{}", fileName, pdfName, outFilePath,
+				officePreviewType);
+		if (OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_IMAGE.equals(officePreviewType)
+				|| OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_ALL_IMAGES.equals(officePreviewType)) {
+			// 当文件不存在时，就去下载
+			if (!fileHandlerService.listConvertedFiles().containsKey(pdfName) || !ConfigConstants.isCacheEnabled()) {
+				log.info("文件下载中");
+				ReturnResponse<String> response = DownloadUtils.downLoad(fileAttribute, fileName);
+				if (response.isFailure()) {
+					log.info("文件下载失败");
+					return otherFilePreview.notSupportedFile(model, fileAttribute, response.getMsg());
+				}
+				outFilePath = response.getContent();
+				if (ConfigConstants.isCacheEnabled()) {
+					// 加入缓存
+					fileHandlerService.addConvertedFile(pdfName, fileHandlerService.getRelativePath(outFilePath));
+				}
+			}
+			List<String> imageUrls = fileHandlerService.pdf2jpg(outFilePath, pdfName, baseUrl);
+			if (imageUrls == null || imageUrls.size() < 1) {
+				return otherFilePreview.notSupportedFile(model, fileAttribute, "pdf转图片异常，请联系管理员");
+			}
+			model.addAttribute("imgurls", imageUrls);
+			model.addAttribute("currentUrl", imageUrls.get(0));
+			if (OfficeFilePreviewImpl.OFFICE_PREVIEW_TYPE_IMAGE.equals(officePreviewType)) {
+				return OFFICE_PICTURE_FILE_PREVIEW_PAGE;
+			} else {
+				return PICTURE_FILE_PREVIEW_PAGE;
+			}
+		} else {
+			// 不是http开头，浏览器不能直接访问，需下载到本地
+			if (url != null && !url.toLowerCase().startsWith("http")) {
+				if (!fileHandlerService.listConvertedFiles().containsKey(pdfName)
+						|| !ConfigConstants.isCacheEnabled()) {
+					ReturnResponse<String> response = DownloadUtils.downLoad(fileAttribute, pdfName);
+					if (response.isFailure()) {
+						return otherFilePreview.notSupportedFile(model, fileAttribute, response.getMsg());
+					}
+					model.addAttribute("pdfUrl", fileHandlerService.getRelativePath(response.getContent()));
+					if (ConfigConstants.isCacheEnabled()) {
+						// 加入缓存
+						fileHandlerService.addConvertedFile(pdfName, fileHandlerService.getRelativePath(outFilePath));
+					}
+				} else {
+					model.addAttribute("pdfUrl", pdfName);
+				}
+			} else {
+				model.addAttribute("pdfUrl", url);
+			}
+		}
+		return PDF_FILE_PREVIEW_PAGE;
+	}
 }
